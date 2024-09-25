fe603ab8a02bb7b6a2ca2d53307b9cf8a30dbf9e
ryanwmichaud <92121030+ryanwmichaud@users.noreply.github.com>
2024-09-25 14:08:48 -0400

    start script

---
+++ backend/run.py
@@ -0,0 +1,27 @@
+
+
+import os
+import subprocess
+import sys
+
+
+
+
+args = []
+
+args.append("flask")
+
+if  (os.getenv("FLASK_APP") == None):
+    args.append("--app")
+    args.append("server")
+
+args.append("run")
+
+args.append("--host=0.0.0.0")
+
+if len(sys.argv) ==2:
+    if sys.argv[1] == "--debug":
+        args.append("--debug")
+
+
+subprocess.run(args)
--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+print("evv",os.getenv("FLASK_APP"))
+
+load_dotenv() #load environment variables from .env
+
 
 app = Flask(__name__)
 
+    
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello</p>"
+def hello_world(): 
+    return "<p>Helo</p>"
+
+
+
